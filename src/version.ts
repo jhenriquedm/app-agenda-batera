b45588f849e3a82d492836a740ca159ddcbe83d6
jhenriquedm <jhenriquedm98@gmail.com>
2026-09-18 13:55:33 +0000

    fix: sincroniza package_name no google-services.json e incrementa versao para v1.0.1

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.0.0';
-export const APP_BUILD_NUMBER = 1;
+export const APP_VERSION = '1.0.1';
+export const APP_BUILD_NUMBER = 2;
 export const APP_NAME = 'Agenda do Batera';
